cc78cc2be260a38fc7ea254ef9a493ad81236a8a
PatrickMck34 <corkscrewhollow@gmail.com>
2024-04-07 14:18:02 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [miles, setMiles] = useState(null);
+  const [gas, setGas] = useState(3.29);
+  let oilChange = 74000
+
+  let tmiles = miles - 74000
+  let tigGas = 3.93
+  let totalNow = (tmiles / 35) * gas
+  let totalOld = (tmiles / 22) * tigGas
+  let saved = (totalOld - totalNow).toFixed(2)
+  if(saved < 0){ saved = 0.00 }
+  while (oilChange <= miles) {
+    oilChange += 10000;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lets See How Much You've Saved so far!</h1>
+      <input
+                    type="Integer"
+                    value={miles}
+                    onChange={(e) => setMiles(e.target.value)}
+                    placeholder="Miles"
+                    className="rounded-xl mt-5 shadow-xl shadow-slate-800 h-fit w-full  border-2 border-slate-700 p-1   justify-center items-center flex bg-gray-100  lg:text-3xl  text-xl"
+                ></input>
+                   
+                     <div>total is = ${saved}</div>
+                     next oil change is at {oilChange}
+                  
     </div>
   );
 }
