78845c13e80c72d5e5cc1a60e5a3d43f0c279df9
PatrickMck34 <corkscrewhollow@gmail.com>
2024-04-10 21:03:44 -0400

    styled for phones

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ const mazda6 = {
 
 
   return (
-    <div className="App">
-      <h1 className="text-4xl font-semibold text-blue-900 mb-3 mt-5">Lets See How Much Randy Has Saved so far!</h1>
+    <div className="App ">
+      <h1 className="text-4xl font-semibold text-blue-900 mb-3 mt-5 items-center flex justify-center">Lets See How Much Randy Has Saved so far!</h1>
       <div className="flex items-center justify-center flex-col">
 
       <input
@@ -39,7 +39,7 @@ const mazda6 = {
                     value={miles}
                     onChange={(e) => setMiles(e.target.value)}
                     placeholder="Miles"
-                    className="rounded-xl mt-5h-fit w-[80%] shadow-md shadow-blue-900 border-2 border-slate-700 p-1   justify-center items-center flex bg-gray-100  lg:text-3xl  text-xl"
+                    className="rounded-xl mt-5h-fit w-[80%]  shadow-md shadow-blue-900 border-2 border-slate-700 p-1   justify-center items-center flex bg-gray-100  lg:text-3xl  text-xl"
                     ></input>
                    
                      <div className="mt-2 font-semibold text-2xl ">total is = ${saved}
@@ -47,27 +47,27 @@ const mazda6 = {
                      <div>
 
                     Maintenance Info:
-                  <div className="border-2 bg-slate-700 shadow-md shadow-blue-700 border-black p-2 flex font-semibold flex-row">
+                  <div className="border-2 items-center justify-center bg-slate-700 shadow-md shadow-blue-700 w-full ml-[15%]  mr-[10%] overflow-auto border-black p-2 flex font-semibold flex-row">
                     <div>
 
                     <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Oil</div>
-                    <div className="border-2 p-3 bg-gray-200  border-black h-5 mt-2 text-center items-center flex justify-center">Oil-Filters</div>
+                    <div className="border-2 p-3 bg-gray-200  border-black mb-7 h-5 mt-2 text-center items-center flex justify-center">Oil-Filters</div>
                     <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Headlights</div>
-                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Low-beams</div>
-                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Spark Plugs</div>
+                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Lows</div>
+                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Spark</div>
                     <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Wipers</div>
-                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Lights</div>
+                    <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center mb-7 flex justify-center">Lights</div>
                     <div className="border-2 p-3 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Air-filter</div>
                     </div>
                     <div className="flex flex-col">
 
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">{mazda6.oil}</div>
-                    <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Mobile1: {mazda6.OilFilter.Mobile1} <p className=" ml-5">Fram: {mazda6.OilFilter.Fram}</p> <p className=" ml-5">K&N: {mazda6.OilFilter.KN}</p></div>
+                    <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-12 mt-2 text-center items-center flex justify-center">Mobile1: {mazda6.OilFilter.Mobile1} <p className=" ml-5"> Fram: {mazda6.OilFilter.Fram}</p> <p className=" ml-5">K&N: {mazda6.OilFilter.KN}</p></div>
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">{mazda6.Headlights}</div>
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">{mazda6.Lowbeams}</div>
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">{mazda6.SparkPlugs}</div>
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">{mazda6.Wipers}</div>
-                    <div className=" ml-5 p-3 border-2 bg-gray-200  border-black h-5 mt-2 text-center items-center flex justify-center">Back up: {mazda6.Lights.BackUp} <p className="ml-5">Trunk: {mazda6.Lights.Trunk}</p><p className="ml-5">Courtesy/License: {mazda6.Lights.CourtesyLicense}</p></div>
+                    <div className=" ml-5 p-3 border-2 bg-gray-200  border-black h-12 mt-2 text-center items-center flex justify-center">Back: {mazda6.Lights.BackUp} <p className="ml-5">Trunk: {mazda6.Lights.Trunk}</p><p className="ml-5">Courtesy/License: {mazda6.Lights.CourtesyLicense}</p></div>
                     <div className=" ml-5 p-3 border-2 bg-gray-200 border-black h-5 mt-2 text-center items-center flex justify-center">Fram: {mazda6.FramAirFilter}</div>
                     </div>
                      
